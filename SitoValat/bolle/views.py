7806b052c9099a680f86b894245b256251ef4070
Kaminari94 <31014911+Kaminari94@users.noreply.github.com>
2025-07-01 16:02:17 +0200

    QuickFix: fix al calcolo data per generazione fatture automatiche.

--- SitoValat/bolle/views.py
+++ SitoValat/bolle/views.py
@@ -2413,7 +2413,8 @@ class CreaFattureAuto(LoginRequiredMixin, View):
         tipo_doc = TipoDocumento.objects.filter(concessionario=conc)
         # Recupera data di oggi e aggiungici un mese
         data_inizio = timezone.now().replace(day=1, month=mese, year=anno, hour=0, minute=0)
-        data_fine = timezone.now().replace(day=1, month=mese, year=anno, hour=23, minute=59) + relativedelta(months=1) - relativedelta(days=1)
+        data_fine = data_inizio + relativedelta(months=1) - relativedelta(days=1)
+        data_fine = datetime.combine(data_fine.date(), datetime.max.time())
         print("Oggi: ", data_inizio, " Ultimo giorno mese precedente: ", data_fine)
         clienti = Cliente.objects.filter(concessionario = conc, tipo_documento_predefinito__in=tipo_doc).order_by("nome")
         numero = 0
@@ -2453,6 +2454,8 @@ class CreaFattureAuto(LoginRequiredMixin, View):
 
             for bolla in bolle_cliente:
                 for riga in bolla.righe.all():
+                    if cliente.nome == "Ricco Group":
+                        print(bolla)
                     articolo = riga.articolo
                     prezzo_pers = PrezziPersonalizzati.objects.filter(articolo=articolo, cliente=cliente).first()
                     if prezzo_pers:
